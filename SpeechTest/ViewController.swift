6133903ce34b1cb440b315ea521bd09822ec320f
YouJCheng <Yu-J.Cheng@udt.tw>
2018-03-31 16:24:04 +0800

    fist commit

---
+++ SpeechTest/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  SpeechTest
+//
+//  Created by Yu-J.Cheng on 2018/3/31.
+//  Copyright © 2018年 YuChienCheng. All rights reserved.
+//
+
+import UIKit
+import Speech
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var transcriptionTextView: UITextView!
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
+
+    var audioPlayer: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        activityIndicatorView.isHidden = true
+        audioPlayer.delegate = self
+    }
+
+
+    @IBAction func transcribeBtnPress(_ sender: UIButton) {
+        activityIndicatorView.isHidden = false
+        activityIndicatorView.startAnimating()
+        requestSpeechAuth()
+    }
+
+
+    private func requestSpeechAuth() {
+        SFSpeechRecognizer.requestAuthorization { (authStates) in
+            if authStates == SFSpeechRecognizerAuthorizationStatus.authorized { 
+                if let path = Bundle.main.url(forResource: "test", withExtension: "m4a"){
+                    do {
+                        let sound = try AVAudioPlayer(contentsOf: path)
+                        self.audioPlayer = sound
+                        sound.play()
+                    } catch {
+                        print("something wrong")
+                    }
+                    let recognizer = SFSpeechRecognizer()
+                    let request = SFSpeechURLRecognitionRequest(url: path)
+                    recognizer?.recognitionTask(with: request, resultHandler: { (result, error) in
+                        if let error = error {
+                            print(error.localizedDescription)
+                        } else {
+                            DispatchQueue.main.async {
+                                self.transcriptionTextView.text = result?.bestTranscription.formattedString
+                            }
+
+                        }
+                    })
+                }
+
+            }
+        }
+    }
+}
+
+extension ViewController: AVAudioPlayerDelegate {
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        player.stop()
+        activityIndicatorView.stopAnimating()
+        activityIndicatorView.isHidden = true
+    }
+}
